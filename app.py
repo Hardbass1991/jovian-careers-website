6766eb1112cbafe175d278a81a2575c44489b32a
bardzodobrze <88125950+Hardbass1991@users.noreply.github.com>
2023-03-30 19:59:45 +0000

    feat: added html, css and api view

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Berlin',
+  'salary': 50000,
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Lyiv',
+  'salary': 30000,
+}, {
+  'id': 3,
+  'title': 'Data Engineer',
+  'location': 'Warsaw',
+  'salary': 40000,
+}]
 
 
 @app.route("/")
 def hello_world():
-  return "Hello ariaaaana"
+  return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
